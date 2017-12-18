4afe77e6cb5cc12e263beb57c35deb459d406283
Luay <luayghazwan@yahoo.com>
2017-12-17 23:30:30 -0800

    Calculator ready for user input with exit option

--- Calculator.NET/Calculator.NET/Program.cs
+++ Calculator.NET/Calculator.NET/Program.cs
@@ -16,7 +16,27 @@ namespace Calculator.NET
                 "\n\tExamples: 45 + 16" +
                 "\n\t          65 * 4 / 531" +
                 "\n\t          49 - 2 + 134 * 648" +
-                "\n\n\n");
+                "\n\n" +
+                "\t\t\t\t You can type `EXIT` any time to leave the application." +
+                "\n\n");
+        }
+
+        private static bool CalculatorOn()
+        {
+            var userInput = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(userInput))
+            {
+                const string message = "Type something! :)";
+                Console.WriteLine(message);
+                return true;
+            }
+
+            if (userInput.ToLower() == "exit")
+                return false;
+
+            //Calculate the user input
+            return true;
         }
 
         public static void ExitAndThankYouMessage()
@@ -53,7 +73,7 @@ namespace Calculator.NET
             bool calculatorOn = true;
             while (calculatorOn)
             {
-                //Make calculation
+                calculatorOn = CalculatorOn();
             }
 
             ExitAndThankYouMessage();
