bc8b2b8607fa3ec1ad5e1cc255d70421b05063d8
Luay <luayghazwan@yahoo.com>
2017-12-17 23:23:23 -0800

    initial commit with main scaffolding

---
+++ Calculator.NET/Calculator.NET/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+namespace Calculator.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+
+            Console.WriteLine("-> Calculate ");
+
+            bool calculatorOn = true;
+            while (calculatorOn)
+            {
+                //Make calculation
+            }
+
+            //Exit message
+        }
+
+    }
+}
